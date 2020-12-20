0425e6fb7de179bce77318f248072097ee54fac8
Hayder Mir <hmir@ciena.com>
2020-12-20 13:01:13 -0500

    Update

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,2 @@
 const myHeading = document.querySelector('h1');
-myHeading.textContent = 'Hello world, I have arrived - Really';
-myHeading.textContent = 'Lets Try this';
+myHeading.textContent = 'Hello world, I have arrived';
